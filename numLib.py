74baa4eb001795b5549ee18a80275887bfa89843
pcmquizon <camillemendoza17@gmail.com>
2016-02-10 18:15:54 +0800

    fixed numToWords function

--- numLib.py
+++ numLib.py
@@ -13,12 +13,14 @@ def numToWords(number):
 	t = (number%100)/10
 	o = number%10
 
+	#checker
 	print "m="+str(million)+" hTH="+str(hTH)+" tTH="+str(tTH)+" oTH="+str(oTH)+" h="+str(h)+" t="+str(t)+" o="+str(o)
 
 
 	if(million>0):
 		#print "one million "
-		sys.stdout.write('one million ')
+		onesNum(million)
+		sys.stdout.write(' million ')
 		
 	if(hTH>0 or tTH>0 or oTH>0):
 	
@@ -33,11 +35,11 @@ def numToWords(number):
 #			onesNum(oTH)
 		threePlaces(hTH, tTH, oTH)
 		#print "thousand "
-		sys.stdout.write('thousand ')
+		sys.stdout.write(' thousand ')
 
 	if(h>0 or t>0 or o>0):
 		threePlaces(h, t, o)
-	elif(o==0):
+	elif(o==0 and hTH==0 and tTH==0 and oTH==0 and million==0 and t==0 and h==0):
 		#print "zero"
 		sys.stdout.write('zero')
 			
@@ -47,38 +49,40 @@ def numToWords(number):
 	return None	
 	
 def onesNum(number):
-	""
+	"Evaluates the parameter number and prints the corresponding word form"
 	if(number==1):
 		#print "one "
-		sys.stdout.write('one ')
+		sys.stdout.write('one')
 	if(number==2):
 		#print "two "
-		sys.stdout.write('two ')
+		sys.stdout.write('two')
 	if(number==3):
 		#print "three "
-		sys.stdout.write('three ')
+		sys.stdout.write('three')
 	if(number==4):
 		#print "four "
-		sys.stdout.write('four ')
+		sys.stdout.write('four')
 	if(number==5):
 		#print "five "
-		sys.stdout.write('five ')
+		sys.stdout.write('five')
 	if(number==6):
 		#print "six "
-		sys.stdout.write('six ')
+		sys.stdout.write('six')
 	if(number==7):
 		#print "seven "
-		sys.stdout.write('seven ')
+		sys.stdout.write('seven')
 	if(number==8):
 		#print "eight "
-		sys.stdout.write('eight ')
+		sys.stdout.write('eight')
 	if(number==9):
 		#print "nine "
-		sys.stdout.write('nine ')
+		sys.stdout.write('nine')
 	return None
 
 	
 def tensNum(number):
+	"Evaluates the parameter number and prints the corresponding word form; \
+	 similar to onesNum() function"
 	if(number==10):
 		#print "ten"
 		sys.stdout.write('ten')
@@ -109,30 +113,42 @@ def tensNum(number):
 	return None
 
 def lessThanTwenty(number):
+	"Evaluates the parameter number and prints the corresponding word form; \
+	 similar to onesNum() and lessThanTwenty() method"
 	if(number==11):
 		sys.stdout.write('eleven ')
 	if(number==12):
 		sys.stdout.write('twelve ')
 	if(number==13):
 		sys.stdout.write('thirteen ')
+	if(number==14):
+		sys.stdout.write('fourteen ')
+	if(number==15):
+		sys.stdout.write('fifteen ')
+	if(number==16):
+		sys.stdout.write('sixteen ')
+	if(number==17):
+		sys.stdout.write('seventeen ')
+	if(number==18):
+		sys.stdout.write('eighteen ')
+	if(number==19):
+		sys.stdout.write('nineteen ')
 	
 def threePlaces(h, t, o):
-		if(h>0):
-			onesNum(h)
-			#print "hundred "
-			sys.stdout.write('hundred ')
-		if(t==1 and (1 < o < 4) ):
-			lessThanTwenty(10+o)
-		elif(t==1 and (5 < o < 10) ):
-			onesNum(o)
-			sys.stdout.write('teen ')
-		elif(t>2 and o>0):
-			tensNum(t)
-			#print "-"
-			sys.stdout.write('-')
-			onesNum(o)
-		elif(t>0):
-			tensNum(t)
-		elif(o>0):
-			onesNum(o)
-		return None
+	"Calls the appropriate functions based on the values of h, t and o"
+	if(h>0):
+		onesNum(h)
+		#print "hundred "
+		sys.stdout.write(' hundred ')
+	if(t==1 and (0 < o < 10) ):
+		lessThanTwenty(10+o)
+	elif(t>1 and o>0):
+		tensNum(t*10)
+		#print "-"
+		sys.stdout.write('-')
+		onesNum(o)
+	elif(t>0):
+		tensNum((t*10)+o)
+	elif(o>0):
+		onesNum(o)
+	return None
